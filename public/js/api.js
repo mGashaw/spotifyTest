45e177aa20d99197785f0d77b1333ac8c17135de
Matt Gashaw <mattgashaw2@gmail.com>
2021-08-03 17:03:51 +0000

    apicontroller added

--- public/js/api.js
+++ public/js/api.js
@@ -1,14 +1,13 @@
 export default class APIController {
 
     constructor() {
-        this.clientID = "";
-        this.clientSecret = "shhh";
-        this.token = ""
+        this.clientId = "x";
+        this.clientSecret = "z";
+        this.token = "N/A";
     }
 
     async getToken() {
-
-        const result = await fetch('', {
+        const result = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
             headers: {
                 'Content-Type' : 'application/x-www-form-urlencoded', 
@@ -22,12 +21,14 @@ export default class APIController {
     }
 
     async getGenres() {
-         const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+        console.log(this.token);
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
             method: 'GET',
             headers: { 'Authorization' : 'Bearer ' + this.token}
         });
 
         const data = await result.json();
+        console.log(data);
         return data.categories.items;       
     }
 
--- public/js/main.js
+++ public/js/main.js
@@ -2,21 +2,43 @@
 import APIController from "/js/api.js"
 
 const api = new APIController();
-
-window.onload = (e) => {
-    console.log('windowLoading...')
-}
-
-function displayGenre() {
-
-}
+api.getToken();
 
 function genreSelected() {
-
+    console.log('made it!');
+    // For now print a list of playlists inside of that genre.
 }
 
-// Now finish getGenre function and then display given genre's to the website page. 
-// Set a button clicker with each gallery card, if clicked print all playlists and we be gucci!
+async function main() {
+    if (api.token === "N/A") {
+        setTimeout(main, 1000);
+        return;
+    } 
+    const genreData = await api.getGenres();
+    genreData.forEach(genre => displayGenreCard(genre));
+}
+main();
+
+function displayGenreCard(genre) {
+    const gallery = document.querySelector("#genreGallery");
+    gallery.innerHTML += `
+        <div class="column is-one-fifth">
+            <div class="card">
+                <div class="card-image">
+                    <p hidden> ${genre.id} </p>
+                    <figure class="image is-4by4">
+                    <img src="${genre.icons[0].url}" alt="Placeholder image">
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <button onclick="genreSelected()">${genre.name}</button>
+                </div>
+            </div>
+        </div>        
+    `;
+}
+
+
 
 /**
  * Genre Card
